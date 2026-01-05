438c3eec3e3b628c73a9d5ed113e9e37aaa88b7a
Xinxin <zhangxx19@mails.tsinghua.edu.cn>
2026-01-05 16:45:10 +0800

    数两侧离子数

---
+++ count_2sides_Mg_TFSI.py
@@ -0,0 +1,33 @@
+import MDAnalysis as mda
+import numpy as np
+import matplotlib.pyplot as plt
+
+filepath = r'E:\Project\57.MgTFSI2_DME_interface\1.800_solvents\7.MD_init2_nvt_5V/'
+topo_file = 'NVEa3.data'
+trj_file = 'NVEe_1_5.lammpsdump'
+sys = mda.Universe(topology=filepath+topo_file, trajectory=filepath+trj_file)
+zmax = max(sys.atoms.positions[:,2])
+zmin = min(sys.atoms.positions[:,2])
+zmid = (zmax+zmin)/2
+mg_resid_select = sys.select_atoms('resid 801 to 820')
+tfsi_resid_select = sys.select_atoms('resid 821 to 860')
+for ts in sys.trajectory[::10]:
+    print(ts.frame)
+    n_mg_bot = 0
+    n_mg_top = 0
+    n_tfsi_bot = 0
+    n_tfsi_top = 0
+    for atom in mg_resid_select:
+        if atom.position[2] < zmid:
+            n_mg_bot += 1
+        if atom.position[2] >= zmid:
+            n_mg_top += 1
+    for atom in tfsi_resid_select:
+        if atom.position[2] < zmid:
+            n_tfsi_bot += 1
+        if atom.position[2] >= zmid:
+            n_tfsi_top += 1
+    n_tfsi_bot = n_tfsi_bot/15
+    n_tfsi_top = n_tfsi_top/15
+    print(f'n_mg_bot: {n_mg_bot},n_mg_top:{n_mg_top}, n_tfsi_bot:{n_tfsi_bot}, n_tfsi_top:{n_tfsi_top}')
+
