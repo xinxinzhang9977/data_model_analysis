d2923442a1d6249706670d899fc47d19ee1cfcda
Xinxin <zhangxx19@mails.tsinghua.edu.cn>
2026-01-05 15:09:26 +0800

    隔10帧输出一帧

--- vasprun2xyz.py
+++ vasprun2xyz.py
@@ -7,6 +7,6 @@ filepath = r'E:\Project\57.MgTFSI2_DME_interface\4.pro_43_6\2.pro_43_6_9_Mg1\1.v
 # 使用二进制模式读取文件
 with open(filepath+'vasprun.xml', 'rb') as f:
     trajectory = read(f, index=':', format='vasp-xml')
-
+trajectory_10 = trajectory[::10]
 # 写入xyz文件
-write(filepath+'output.xyz', trajectory, format='xyz')
+write(filepath+'pro_43_6_9_Mg1-pos-1.xyz', trajectory_10, format='xyz')
